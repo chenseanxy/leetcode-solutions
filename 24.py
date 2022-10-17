078b5aa9d75db35f15aa7e7af3ac96be9a253460
Xinyang Chen <chenxy1999728@126.com>
2022-10-18 00:44:17 +0800

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,13 @@
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        res = head.next
+        prev = None
+        while head and head.next and head.next:
+            ptr1, ptr2, ptr3 = head, head.next, head.next.next
+            if prev:
+                prev.next = ptr2
+            prev = ptr1
+            head, ptr1.next, ptr2.next = ptr3, ptr3, ptr1
+        return res
